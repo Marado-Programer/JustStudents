89d09a777346f0e1bdad644a0b3062ba93295cff
marado <78569959+Marado-Programmer@users.noreply.github.com>
2022-04-26 22:57:57 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 // ==UserScript==
-// @name        JustStudents
+// @name        Just Students
 // @namespace   https://github.com/Marado-Programer/
-// @match       http*://oghma.epcc.pt/courses/*/subscriptions
+// @match       http*://oghma.epcc.pt/*
 // @grant       none
 // @version     1.0
 // @author      al220007@epcc.pt
 // @description Remove students that aren't students anymore
 // ==/UserScript==
 
-document.querySelectorAll("li.student").forEach( student => {
-		if (!student.classList.contains("active"))
-			student.remove() 
-} );
+document.querySelectorAll("li.student, tr.student").forEach(student => {
+  if (!student.classList.contains("active"))
+    student.remove();
+});
 
+document.querySelectorAll("table.evaluations tr").forEach((student, i) => {
+  if (i != 0 && !student.classList.contains("active"))
+    student.remove();
+});
