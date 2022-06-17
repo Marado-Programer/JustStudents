a73081c6c455ffdb0bf7060b153de645cd6e8da7
marado <78569959+Marado-Programmer@users.noreply.github.com>
2022-06-17 16:05:32 +0100

    Best notes first for ego boost

--- index.js
+++ index.js
@@ -8,12 +8,26 @@
 // @description Remove students that aren't students anymore
 // ==/UserScript==
 
+const url = window.location.href;
+
 document.querySelectorAll("li.student, tr.student").forEach(student => {
   if (!student.classList.contains("active"))
     student.remove();
 });
 
-document.querySelectorAll("table.evaluations tr").forEach((student, i) => {
-  if (i != 0 && !student.classList.contains("active"))
-    student.remove();
-});
+if (url.match(/units/)) {
+  document.querySelectorAll("table.evaluations tr").forEach((student, i) => {
+    if (i != 0 && !student.classList.contains("active"))
+      student.remove();
+  });
+} else if (url.match(/users/)) {
+  const evaluationsTable = document.getElementsByClassName("evaluations")[1],
+        evaluations = document.querySelectorAll("table.evaluations tr.evaluation");
+  const orderedEvaluations = [].slice.call(evaluations).sort((x, y) => {
+    return x.childNodes[5].innerHTML - y.childNodes[5].innerHTML;
+  });
+  
+  evaluations.forEach(i => i.remove());
+  
+  orderedEvaluations.reverse().forEach(i => evaluationsTable.append(i));
+}
