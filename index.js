7575c2947183f80be644607831d2d2691fcb2dbc
marado <78569959+Marado-Programmer@users.noreply.github.com>
2023-04-18 03:00:09 +0100

    and order the classroom

--- index.js
+++ index.js
@@ -10,37 +10,34 @@
 
 const url = window.location.href;
 
-document.querySelectorAll("li.student, tr.student").forEach(student => {
-  if (!student.classList.contains("active"))
-    student.remove();
+document.querySelectorAll("li.student, tr.student").forEach((student) => {
+  if (!student.classList.contains("active")) student.remove();
 });
 
 if (url.match(/units/)) {
   document.querySelectorAll("table.evaluations tr").forEach((student, i) => {
-    if (i != 0 && !student.classList.contains("active"))
-      student.remove();
+    if (i != 0 && !student.classList.contains("active")) student.remove();
   });
   const evaluationsTable = document.getElementsByClassName("evaluations")[1],
     evaluations = document.querySelectorAll("table.evaluations tr.active");
-  const orderedEvaluations = [].slice.call(evaluations).sort((x, y) => {
-    if (x.querySelector(".number")) {
-      return x.querySelector(".number").childNodes[0].nodeValue - y.querySelector(".number").childNodes[0].nodeValue;
-    } else {
-      return x.querySelector("td:nth-child(4)").innerText - y.querySelector("td:nth-child(4)").innerText;
-    }
-  });
+  const orderedEvaluations = [].slice.call(evaluations).sort((x, y) =>
+    x.querySelector(".number")
+      ? (x.querySelector(".number").childNodes[0].nodeValue -
+        y.querySelector(".number").childNodes[0].nodeValue)
+      : (x.querySelector("td:nth-child(4)").innerText -
+        y.querySelector("td:nth-child(4)").innerText)
+  );
 
-  evaluations.forEach(i => i.remove());
+  evaluations.forEach((i) => i.remove());
 
-  orderedEvaluations.forEach(i => evaluationsTable.append(i));
+  orderedEvaluations.forEach((i) => evaluationsTable.append(i));
 } else if (url.match(/users/)) {
   const evaluationsTable = document.getElementsByClassName("evaluations")[1],
-        evaluations = document.querySelectorAll("table.evaluations tr.evaluation");
+    evaluations = document.querySelectorAll("table.evaluations tr.evaluation");
 
   const date = /(\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2} UTC)/;
 
   const orderedEvaluations = [].slice.call(evaluations).sort((x, y) => {
-
     const xDate = new Date(date.exec(x.childNodes[15].title)[1]);
     const yDate = new Date(date.exec(y.childNodes[15].title)[1]);
 
@@ -48,41 +45,124 @@ if (url.match(/units/)) {
     return x.childNodes[5].innerHTML - y.childNodes[5].innerHTML;
   });
 
-  const avg = orderedEvaluations.reduce((x, y) => parseInt(typeof x === "number" ? x : x.querySelector("td:nth-child(3)").innerText, 10) + parseInt(typeof y === "number" ? y : y.querySelector("td:nth-child(3)").innerText, 10));
+  const avg = orderedEvaluations.reduce((x, y) =>
+    parseInt(
+      typeof x === "number" ? x : x.querySelector("td:nth-child(3)").innerText,
+      10,
+    ) + parseInt(
+      typeof y === "number" ? y : y.querySelector("td:nth-child(3)").innerText,
+      10,
+    )
+  );
 
-  evaluations.forEach(i => i.remove());
+  evaluations.forEach((i) => i.remove());
 
-  //orderedEvaluations.reverse().forEach(i => evaluationsTable.append(i));
-  orderedEvaluations.forEach(i => evaluationsTable.append(i));
+  orderedEvaluations.forEach((i) => evaluationsTable.append(i));
   const avgtr = document.createElement("tr");
-avgtr.append(document.createElement("td"));
-avgtr.append(document.createElement("td"));
+  avgtr.append(document.createElement("td"));
+  avgtr.append(document.createElement("td"));
   const avgtd = document.createElement("td");
   const b = document.createElement("b");
-b.append(document.createTextNode(`Average: ${avg / orderedEvaluations.length}`));
+  b.append(
+    document.createTextNode(`Average: ${avg / orderedEvaluations.length}`),
+  );
   avgtd.append(b);
   avgtr.append(avgtd);
-
-avgtr.append(document.createElement("td"));
-avgtr.append(document.createElement("td"));
-
-avgtr.append(document.createElement("td"));
-avgtr.append(document.createElement("td"));
-
-avgtr.append(document.createElement("td"));
-avgtr.append(document.createElement("td"));
+  avgtr.append(document.createElement("td"));
+  avgtr.append(document.createElement("td"));
+  avgtr.append(document.createElement("td"));
+  avgtr.append(document.createElement("td"));
+  avgtr.append(document.createElement("td"));
+  avgtr.append(document.createElement("td"));
   evaluationsTable.append(avgtr);
 } else if (url.match(/\/courses\/?$/)) {
-  Promise.all([].slice.call(document.querySelectorAll("#main > div > div.span10.main > table > tbody > tr a")).map(i => fetch(`${i.href}/evaluations`).then(res => res.text()).then(course => {
-    const you_start = course.indexOf('<tr class="active me">');
-      const you = course.substring(you_start, course.indexOf("</tr>", you_start)).trim();
-      const rate_start = you.indexOf('<td class="number">');
-      const matches = you.substring(rate_start, you.indexOf("</td>", rate_start)).match(/(\d+\.\d+).*\d+\/(\d+)/);
-    if (matches == null) {
-      return;
-    }
-    const [ , rate, _modules ] = matches;
+  Promise.all(
+    [].slice.call(
+      document.querySelectorAll(
+        "#main > div > div.span10.main > table > tbody > tr a",
+      ),
+    ).map((i) =>
+      fetch(`${i.href}/evaluations`).then((res) => res.text()).then(
+        (course) => {
+          const you_start = course.indexOf('<tr class="active me">');
+          const you = course.substring(
+            you_start,
+            course.indexOf("</tr>", you_start),
+          ).trim();
+          const rate_start = you.indexOf('<td class="number">');
+          const matches = you.substring(
+            rate_start,
+            you.indexOf("</td>", rate_start),
+          ).match(/(\d+\.\d+).*\d+\/(\d+)/);
+          if (matches == null) return;
+          const [, rate, _modules] = matches;
+          return rate;
+        },
+      )
+    ),
+  ).then((rates) => rates.filter((i) => typeof i === "string")).then((rates) =>
+    rates.reduce((x, y) => parseFloat(x, 10) + parseFloat(y, 10)) / rates.length
+  ).then((avg) => console.log(avg));
+} else if (url.match("/courses/")) {
+  (async () => {
+    const pages = [].slice.call(
+      document.querySelectorAll(
+        "#main > div > div.span10.main > ul > li > a:nth-child(1)",
+      ),
+    ).map((
+      i,
+    ) => [i, fetch(`${i.href}/evaluations/`).then((res) => res.text())]);
 
-    return rate;
-  }))).then(rates => rates.filter(i => typeof i === "string")).then(rates =>  rates.reduce((x, y) => parseFloat(x, 10) + parseFloat(y, 10)) / rates.length ).then(avg => console.log(avg));
+    const unsorted = [];
+
+    for (const [url, p] of pages) {
+      const page = await p;
+      const dummy = document.createElement("div");
+      const start = page.indexOf("<table");
+      dummy.innerHTML = page.substring(
+        start,
+        page.indexOf("</table>", start),
+      ).trim();
+
+      const evaluationsTable = dummy.getElementsByClassName("evaluations")[0],
+        evaluations = dummy.querySelectorAll(
+          "table.evaluations tr.evaluation",
+        );
+
+      const avg = [].slice.apply(evaluations).reduce((x, y) =>
+        parseInt(
+          typeof x === "number"
+            ? x
+            : x.querySelector("td:nth-child(3)").innerText,
+          10,
+        ) + parseInt(
+          typeof y === "number"
+            ? y
+            : y.querySelector("td:nth-child(3)").innerText,
+          10,
+        ), 0);
+
+      unsorted.push([url, (avg / evaluations.length).toFixed(3)]);
+    }
+
+    const lisorted = unsorted.sort(([, x], [, y]) => x - y).map(
+      ([href, avg]) => {
+        const url = (new URL(href).pathname).replace(/\/?evaluations\/?/, "");
+        const student = document.querySelector(
+          `#main > div > div.span10.main > ul > li:has(a[href="${url}"])`,
+        );
+        const n = student.querySelector("span");
+        if (n != null) {
+          n.innerText += ` (${avg})`;
+        }
+        return student;
+      },
+    );
+
+    const list = document.querySelector("#main > div > div.span10.main > ul");
+    console.log(list);
+    [].slice.apply(list.querySelectorAll("li")).forEach((i) => i.remove());
+
+    lisorted.forEach((i) => list.append(i));
+  })();
 }
