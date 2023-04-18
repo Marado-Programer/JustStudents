1477128c22b8306c8db920b4310495d99455fcba
marado <78569959+Marado-Programmer@users.noreply.github.com>
2023-04-18 01:22:37 +0100

    Update index.js
    avg

--- index.js
+++ index.js
@@ -48,8 +48,41 @@ if (url.match(/units/)) {
     return x.childNodes[5].innerHTML - y.childNodes[5].innerHTML;
   });
 
+  const avg = orderedEvaluations.reduce((x, y) => parseInt(typeof x === "number" ? x : x.querySelector("td:nth-child(3)").innerText, 10) + parseInt(typeof y === "number" ? y : y.querySelector("td:nth-child(3)").innerText, 10));
+
   evaluations.forEach(i => i.remove());
 
   //orderedEvaluations.reverse().forEach(i => evaluationsTable.append(i));
   orderedEvaluations.forEach(i => evaluationsTable.append(i));
+  const avgtr = document.createElement("tr");
+avgtr.append(document.createElement("td"));
+avgtr.append(document.createElement("td"));
+  const avgtd = document.createElement("td");
+  const b = document.createElement("b");
+b.append(document.createTextNode(`Average: ${avg / orderedEvaluations.length}`));
+  avgtd.append(b);
+  avgtr.append(avgtd);
+
+avgtr.append(document.createElement("td"));
+avgtr.append(document.createElement("td"));
+
+avgtr.append(document.createElement("td"));
+avgtr.append(document.createElement("td"));
+
+avgtr.append(document.createElement("td"));
+avgtr.append(document.createElement("td"));
+  evaluationsTable.append(avgtr);
+} else if (url.match(/\/courses\/?$/)) {
+  Promise.all([].slice.call(document.querySelectorAll("#main > div > div.span10.main > table > tbody > tr a")).map(i => fetch(`${i.href}/evaluations`).then(res => res.text()).then(course => {
+    const you_start = course.indexOf('<tr class="active me">');
+      const you = course.substring(you_start, course.indexOf("</tr>", you_start)).trim();
+      const rate_start = you.indexOf('<td class="number">');
+      const matches = you.substring(rate_start, you.indexOf("</td>", rate_start)).match(/(\d+\.\d+).*\d+\/(\d+)/);
+    if (matches == null) {
+      return;
+    }
+    const [ , rate, _modules ] = matches;
+
+    return rate;
+  }))).then(rates => rates.filter(i => typeof i === "string")).then(rates =>  rates.reduce((x, y) => parseFloat(x, 10) + parseFloat(y, 10)) / rates.length ).then(avg => console.log(avg));
 }
